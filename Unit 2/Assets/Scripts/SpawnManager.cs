3ccb5215606eac6e1f74a117b1a3812db1a0a9ce
VijayNamboodiri <85519890+VijayNamboodiri@users.noreply.github.com>
2023-05-23 23:01:55 -0700

    2.4.2
    -Added autospawn
    -startdelay, spawn interval

--- Unit 2/Assets/Scripts/SpawnManager.cs
+++ Unit 2/Assets/Scripts/SpawnManager.cs
@@ -7,24 +7,30 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
-    
+    public float startDelay = 2;
+    public float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
        
-        if(Input.GetKeyDown(KeyCode.S))//'GetKeyDown' Tells the computer to detect if a specific key(in this case 'S') is pressed down
-        {
+       
+
+    }
+
+
+    public void SpawnAnimal()
+    {
+          
+        int animalIndex = Random.Range(0,animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0, spawnPosZ);
             
-            int animalIndex = Random.Range(0,animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0, spawnPosZ);
-            
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0,0,20), animalPrefabs[animalIndex].transform.rotation);
-        }
+        Instantiate(animalPrefabs[animalIndex], new Vector3(0,0,20), animalPrefabs[animalIndex].transform.rotation);
     }
 }
+
